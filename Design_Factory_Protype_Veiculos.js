5924eb33d846a3df7ba6ef3cb97cd4dc74af0bfa
Leandro <105015643+leandrojp360@users.noreply.github.com>
2022-11-10 18:42:50 -0300

    Create Design_Factory_Protype_Veiculos.js

---
+++ Design_Factory_Protype_Veiculos.js
@@ -0,0 +1,29 @@
+public abstract class Veiculo {
+    public int numRodas;
+    public String fabricante;
+    public string modelo;
+    public String cor;
+
+    public Veiculo () {
+    }
+}
+public class Carro = function() {
+    this.numRodas   = 4;
+    this.fabricante = 'fiat';
+    this.modelo       = 'Palio';
+    this.cor        = 'preto';
+}
+Carro.prototype = function() {
+
+var go = function() {
+};
+
+var stop = function() {
+};
+
+return {
+pressBrakePedal: stop,
+pressGasPedal: go
+}
+
+}();
